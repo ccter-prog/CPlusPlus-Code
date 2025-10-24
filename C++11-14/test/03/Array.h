ab6f6d3a9267a5ff8d1298fc8440f0b7b40fc98c
Ghost <Ghost@qq.com>
2025-10-25 00:26:02 +0800

    重构动态数组

--- C++11-14/test/03/Array.h
+++ C++11-14/test/03/Array.h
@@ -1,83 +1,68 @@
 #ifndef __Array_H__
 #define __Array_H__
 
+#include <cstddef>
+#include <memory>
+#include <utility>
+
 template <typename T>
 class Array
 {
     public:
         // 以下为特殊成员函数
-        Array() : m_arr(nullptr), m_size(1), m_index(0) {}
-        Array(const T yuansu);
+        Array() : m_data(nullptr), m_capacity(1), m_size(0) {}
+        Array(const T value) : m_data(nullptr), m_capacity(1), m_size(0) { push_back(value); }
         Array(const Array& obj);
-        virtual ~Array();
+        ~Array() {}
         
         // 以下为运算符重载
-        T& operator[](int index) { return m_arr[index]; }
-        T& operator*() { return m_arr[0]; }
-        T& operator*(const int& p) { return m_arr[p]; }
-        Array& operator=(Array& obj);
+        T& operator[](size_t index) { return m_data[index]; }
+        Array& operator=(const Array& obj);
         bool operator==(const Array& obj);
     public:
         // 以下为普通函数
-        Array& push_back(const T num);
-        int length(const Array& obj) { return obj.m_size; }
+        Array& push_back(const T value);
+        size_t length() { return m_size; }
     private:
-        T* m_arr;
-        int m_size;
-        int m_index;
+        void reserve(size_t new_capacity);
+    private:
+        std::unique_ptr<T[]> m_data;  // 数据存储指针
+        size_t m_capacity;  // 数组容量
+        size_t m_size;  // 当前元素数量
 };
 
 template <typename T>
-inline Array<T>::Array(const T yuansu) : m_arr(nullptr), m_size(1), m_index(0)
+inline Array<T>::Array(const Array<T>& obj) : m_data(nullptr), m_size(obj.m_size), m_capacity(1)
 {
-    push_back(yuansu);
+    reserve(obj.m_capacity);
+    for (size_t i = 0; i < m_size; i++)
+    {
+        m_data[i] = obj.m_data[i];
+    }
 }
 
 template <typename T>
-inline Array<T>::Array(const Array<T>& obj)
+inline Array<T>& Array<T>::operator=(const Array<T>& obj)
 {
+    m_data.reset();
+    reserve(obj.m_capacity);
     m_size = obj.m_size;
-    m_index = obj.m_index;
-    m_arr = new T[(unsigned long)m_size];
-    for (int i = 0; i < m_size; i++)
+    for (size_t i = 0; i < m_size; i++)
     {
-        m_arr[i] = obj.m_arr[i];
-    }
-}
-
-template <typename T>
-inline Array<T>& Array<T>::operator=(Array<T>& obj)
-{
-    if (obj.m_arr)
-    {
-        delete[] m_arr;
-        obj.m_arr = nullptr;
-    }
-    obj.m_size = m_size;
-    obj.m_arr = new T[(unsigned long)obj.m_size];
-    for (int i = 0; i < m_size; i++)
-    {
-        obj.m_arr[i] = m_arr[i];
+        m_data[i] = obj.m_data[i];
     }
     return *this;
 }
 
 template <typename T>
-inline Array<T>& Array<T>::push_back(const T num)
+inline Array<T>& Array<T>::push_back(const T value)
 {
-    if (!m_arr || m_index == m_size)
+    if (!m_data || m_capacity == m_size)
     {
-        T* p = m_arr;
-        m_size *= 2;
-        m_arr = new T[(unsigned long)m_size];
-        for (int i = 0; p && i < m_size; i++)
-        {
-            m_arr[i] = p[i];
-        }
-        delete[] p;
+        reserve(m_capacity * 2);
     }
-    m_arr[m_index] = num;
-    m_index++;
+    m_data[m_size] = value;
+    m_size++;
     return *this;
 }
 
@@ -85,25 +70,50 @@ template <typename T>
 inline bool Array<T>::operator==(const Array<T>& obj)
 {
     bool ret = true;
-    for (int i = 0; m_index == obj.m_index && i < m_index; i++)
+    if (m_size != obj.m_size)
     {
-        if (m_arr[i] != obj.m_arr[i])
+        ret = false;
+    }
+    else
+    {
+        size_t i;
+        for (i = 0; i < m_size && m_data[i] == obj.m_data[i]; i++);
+        if (i != m_size) // 循环结束后如果两边的值相等的话，那么i应该等于m_size
         {
             ret = false;
-            break;
         }
     }
     return ret;
 }
 
 template <typename T>
-inline Array<T>::~Array()
+inline void Array<T>::reserve(size_t new_capacity)
 {
-    if (m_arr)
+    if (new_capacity <= m_capacity)  // 判断新的容量是否比旧的容量大
     {
-        delete[] m_arr;
+        return;
     }
-    m_arr = nullptr;
+    std::unique_ptr new_data = std::make_unique<T[]>(new_capacity);
+    if (m_data)  // 如果m_data不是空指针的话才需要数据迁移，循环条件i < m_size确保了不是空指针但里面没有元素的情况，这种概率很小
+    {
+        if constexpr (std::is_nothrow_move_constructible_v<T>)  // 如果类型支持不抛异常的移动构造，则使用移动语义
+        {
+            for (size_t i = 0; i < m_size; i++)
+            {
+                new_data[i] = std::move(m_data[i]);
+            }
+        }
+        else
+        {
+            // 否则使用复制
+            for (size_t i = 0; i < m_size; i++)
+            {
+                new_data[i] = m_data[i];
+            }
+        }
+    }
+    m_data.swap(new_data);
+    m_capacity = new_capacity;
 }
 
 #endif
